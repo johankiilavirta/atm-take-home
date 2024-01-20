23f73f63b70829d809eed6edebe7d94620dd13fd
Johan Kiilavirta <johan@Johans-MacBook-Pro.local>
2024-01-19 21:41:37 -0500

    Starting tests

---
+++ tests.py
@@ -0,0 +1,21 @@
+from atm_controller import ATM_Controller
+TEST_CARD_1 = "0000-0000-0000-0000"
+TEST_PIN_1 = "0000"
+
+
+def withdraw_from_empty_atm(controller : ATM_Controller):
+    controller.insert_card(TEST_CARD_1)
+    controller.insert_pin(TEST_PIN_1)
+    controller.select_account("savings")
+
+    try:
+        controller.withdraw(1)
+    except:
+        return True
+    
+    return False
+
+results = []
+results.append(withdraw_from_empty_atm(ATM_Controller(0, 100)))
+    
+
